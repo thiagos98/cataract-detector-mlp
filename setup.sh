1c352a540cdb7896966f1d9e3c10208a372f04a5
Thiago Santos Borges <tsb.snf16@uea.edu.br>
2021-07-27 20:13:57 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"tsb.snf16@uea.edu.br\"\n\
+" > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
+enableCORS=false\n\
 port = $PORT\n\
-enableCORS = false\n\
-\n\
 " > ~/.streamlit/config.toml
